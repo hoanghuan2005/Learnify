45109d6d5761218875d90b92cd68c574e6c4116c
hoanghuan2005 <hohuan2005uh@gmail.com>
2025-09-07 23:06:40 +0700

    complete login signup logout verifyOtp

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,16 @@
-import axios from 'axios'
+import axios from "axios";
 
-const api = axios.create({
-  baseURL: 'http://localhost:8080',        
-  // withCredentials: true,   
-})
+export const axiosInstance = axios.create({
+  baseURL: "http://localhost:8080",
+  withCredentials: true, // send cookies with requests
+});
 
-export default api
+axiosInstance.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+}, (error) => Promise.reject(error));
+
+export default axiosInstance;
